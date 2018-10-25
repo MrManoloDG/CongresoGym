83b1b37ac638eb1e2f0f198f938aa5b432d57475
Manuel Díaz Gil <FrkWTF@gmail.com>
2018-10-25 13:00:46 +0200

    Programa - cambio de imagenes y mostrar detalle articulo on click

--- js/programa.js
+++ js/programa.js
@@ -6,5 +6,9 @@ $(document).ready(function(){
       $("#titulo").text($(this).find("h3").text());
       $("#horario").text($(this).find("span").text());
       $("#articulo").text($(this).find(".descripcion").text());
+	  $("#imagen").attr('src',$(this).find("img").attr('src'));
+
+	  var scrollPos =  $("html").offset().top;
+ 	  $(window).scrollTop(scrollPos);
     });
 });
