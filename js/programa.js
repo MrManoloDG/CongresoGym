1ab6d3c35d3bd29933abb7b3a6d113568f2333f2
Manuel Díaz Gil <FrkWTF@gmail.com>
2018-11-04 18:58:48 +0100

    Cambios añadiendo mediaquery haciendo responsive

--- js/programa.js
+++ js/programa.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
     $(".prog").click(function() {
       $("#titulo").text($(this).find("h3").text());
       $("#horario").text($(this).find("span").text());
-      $("#articulo").text($(this).find(".descripcion").text());
-	  $("#imagen").attr('src',$(this).find("img").attr('src'));
+      $("#articulo").html($(this).find(".descripcion").html());
+  	  $("#imagen").attr('src',$(this).find("img").attr('src'));
 
-	  var scrollPos =  $("html").offset().top;
- 	  $(window).scrollTop(scrollPos);
+  	  var scrollPos =  $("html").offset().top;
+   	  $(window).scrollTop(scrollPos);
     });
 });
